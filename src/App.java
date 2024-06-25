d34e5127e58520f5f0c0463e7760cc51dcf9c67c
Vladimir Muraito <vl.muraito@mail.ru>
2024-06-25 19:07:26 +1000

    Прошу проверить ДЗ. Извините за опоздание.

--- src/App.java
+++ src/App.java
@@ -1,4 +1,33 @@
+import exception.WrongLoginException;
+import exception.WrongPasswordException;
+
 public class App {
+    private static final String VALID_SYMBOLS = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890_";
+
     public static void main(String[] args) {
+        try {
+            authenticateUser("s3__EWR1", "32313", "32313");
+            System.out.println("Добро пожаловать!");
+        } catch (WrongLoginException wrongLoginException) {
+            throw new WrongLoginException();
+        } catch (WrongPasswordException wrongPasswordException) {
+            throw new WrongPasswordException();
+        }
+    }
+
+    private static void authenticateUser(String login, String password, String confirmPassword) {
+        for (int i = 0; i < login.length(); i++) {
+            if (!VALID_SYMBOLS.contains(Character.toString(login.charAt(i))) || login.length() > 20) {
+                throw new WrongLoginException();
+            }
+        }
+        for (int i = 0; i < password.length(); i++) {
+            if (!VALID_SYMBOLS.contains(Character.toString(password.charAt(i))) || password.length() > 20) {
+                throw new WrongPasswordException();
+            }
+        }
+        if (!password.equals(confirmPassword)) {
+            throw new WrongPasswordException();
+        }
     }
 }
